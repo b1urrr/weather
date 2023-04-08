e74632a0db17dfbbc17a8d90078be55c5ddc2bc0
b1urrr <kristiangogow@gmail.com>
2023-04-08 19:34:25 +0300

    Homepage design updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import { Navbar } from "./components";
+import { Home } from "./containers";
 
 function App() {
   return (
+    <>
     <Navbar />
+    <Home />
+    </>
   );
 }
 
---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='wg__home section__padding'>
+
+        <div className='wg__home-current'>
+
+        </div>
+
+
+        <div className='wg__home-main'>
+        <div className='wg__home-main_menu'>
+            <button>В момента</button>
+            <button>3 часа</button>
+            <button>5 дни</button>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
