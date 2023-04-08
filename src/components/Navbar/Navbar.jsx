9dbb9f5189bd9404bcc898585eb9f68562a52e0e
b1urrr <kristiangogow@gmail.com>
2023-04-08 17:53:10 +0300

    Navbar design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { BsUmbrella, BsSearch } from "react-icons/bs";
-import './navbar.css'
+import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="wg__navbar">
       <div className="wg__navbar-logo">
-        <BsUmbrella size={60} color="rgb(255, 162, 23)"/>
+        <BsUmbrella size={60} color="rgb(255, 162, 23)" />
         <p>Времето</p>
       </div>
       <div className="wg__navbar-links">
-        <a><p>България</p></a>
+        <p>България</p>
         <p>|</p>
-        <a><p>Европа</p></a>
+        <p>Европа</p>
         <p>|</p>
-        <input/>
-        <button><BsSearch size={20} color="rgb(243, 243, 243)"/></button>
+        <input />
+        <button>
+          <BsSearch size={20} color="rgb(243, 243, 243)" />
+        </button>
       </div>
     </div>
   );
