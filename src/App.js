4415d6db844a2b1de47b905f313117324ce74461
b1urrr <kristiangogow@gmail.com>
2023-04-08 12:25:07 +0300

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+/* TODO: 
+
+  SETUP:
+    TODO: GitHub/Firebase CI/CD
+    TODO: How to hide API key
+
+
+  MVP:
+
+    Current - Get info from simple-weather-app
+    3h - https://openweathermap.org/forecast5
+    5days - https://openweathermap.org/forecast5
+    Bulgaria / Europe / World - figure it out
+
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>ASD</h1>
+    </>
   );
 }
 
