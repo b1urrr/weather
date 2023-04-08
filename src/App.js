911ab8faa459ce5785bbc122c2c27bad2b0e0a9c
b1urrr <kristiangogow@gmail.com>
2023-04-08 17:50:00 +0300

    Navbar design done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
 import "./App.css";
-
-/* TODO: 
-
-  SETUP:
-
-    TODO: GitHub/Firebase CI/CD
-    TODO: How to hide API key
-
-  STYLING:
-  
-    Try using SASS
-
-  MVP:
-
-    Current - Get info from simple-weather-app
-    3h - https://openweathermap.org/forecast5
-    5days - https://openweathermap.org/forecast5
-    Bulgaria / Europe / World - figure it out
-
-
-  Bonus Features:
-    
-    Interactive Map to select cities
-    A list of current weather in European Capitals
-
-*/
+import { Navbar } from "./components";
 
 function App() {
   return (
-    <>
-      <h1>DFF</h1>
-    </>
+    <Navbar />
   );
 }
 
